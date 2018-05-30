1f4bad66ac69fe9f35f1e2301392d9ee68aa9aa2
h3r0xfr <adrien.cabaud@numericable.fr>
2018-05-30 13:01:02 +0200

    tinymce locale + design fixes

---
+++ public/assets/js/global/tinymce.js
@@ -0,0 +1,1 @@
+tinymce.init({selector:".tinyedit",language:"fr_FR",height:300,plugins:"advlist autolink link image lists charmap print preview"});
---
+++ src/js/global/tinymce.js
@@ -0,0 +1,6 @@
+tinymce.init({
+    selector: '.tinyedit',
+    language: 'fr_FR',
+    height: 300,
+    plugins: 'advlist autolink link image lists charmap print preview'
+});
